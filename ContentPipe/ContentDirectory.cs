9857a6982ce1c7399533c569c6d96a453f953b12
khhs167 <khhs1671@gmail.com>
2023-02-11 19:51:44 +0100

    polished stuff up

--- ContentPipe/ContentDirectory.cs
+++ ContentPipe/ContentDirectory.cs
@@ -3,10 +3,17 @@ using System.IO.Compression;
 
 namespace ContentPipe;
 
+/// <summary>
+/// A packed content directory(a .cpkg file), used by the Content class.
+/// </summary>
 public struct ContentDirectory
 {
 	private Dictionary<string, ContentLump> contentLumps = new Dictionary<string, ContentLump>();
-
+	
+	/// <summary>
+	/// Fetch a content lump, returns null if it is not available
+	/// </summary>
+	/// <param name="name">The path to the resource</param>
 	public ContentLump? this[string name]
 	{
 		get
@@ -16,7 +23,13 @@ public struct ContentDirectory
 			return null;
 		}
 	}
-
+	
+	/// <summary>
+	/// Compress a directory into a .cpkg file. It will be stored in the same directory as the source directory,
+	/// if path is "Content/TextData", it will output a file called "Content/TextData.cpkg". This file can then be loaded in the Content class with
+	/// <code>Content.LoadDirectory("Content/TextData");</code>
+	/// </summary>
+	/// <param name="path">The path to the directory to compress</param>
 	public static void CompressDirectory(string path)
 	{
 		
@@ -46,7 +59,11 @@ public struct ContentDirectory
 		fileStream.Close();
 
 	}
-
+	
+	/// <summary>
+	/// Load a packed content directory(a .cpkg file) from a path
+	/// </summary>
+	/// <param name="path">The path to the file, excluding the extension</param>
 	public ContentDirectory(string path)
 	{
 
@@ -67,16 +84,27 @@ public struct ContentDirectory
 	}
 }
 
+/// <summary>
+/// A content lump, or in other words, a named byte array. Used to store data in a cpkg file.
+/// </summary>
 [MessagePackObject]
 public struct ContentLump
 {
+	/// <summary>
+	/// The name of the content lump
+	/// </summary>
 	[Key(0)]
 	public string Name;
 	
+	/// <summary>
+	/// The data of the ContentLump
+	/// </summary>
 	[Key(1)]
 	public byte[] Data = Array.Empty<byte>();
 
-
+	/// <summary>
+	/// Create a content lump with 0:ed fields.
+	/// </summary>
 	public ContentLump()
 	{
 		Name = "";
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ContentPipe
-TODO: Add something here
+ContentPipe is a very basic content pipeline inspired by a series of things such as 
+the MonoGame Content Pipeline, GMod and its game mounting and more.
+
+The usage is extensively documented using doxygen comments, so don't expect the best
+documentation outside of your IDE and the source code.
+
+Basic example:
+```csharp
+using ContentPipe;
+
+ContentDirectory.CompressDirectory("Content");
+Content.LoadDirectory("Content");
+
+string someJsonData = Content.LoadString("file.json");
+
+// This step is far from mandatory, but can be used to free up memory or
+// if you want to do something like having consistent file names between parts
+// of the game(use content directories for e.g levels etc)
+
+Content.UnloadDirectory("Content");
+```
